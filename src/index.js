64d7e36c1138cc1a82d33effd927bb127ff0f9dd
Komar0ff <binary.reptile@gmail.com>
2018-10-20 16:22:55 +0300

    remove trash

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { render } from 'react-dom';
+import App from './App/App';
 
-import App from './App.js';
-
-// let store = configureStore();
-
-
-ReactDOM.render(
-	// <Provider store={store}>
-		<Router history={browserHistory}>
-			<Route path="/" component={App}>
-					<Route path='/2048' />
-					<Route path='/Tictactoe' />
-					<Route path='/Ships' />
-			</Route>
-		</Router>,
-	// </Provider>,
-	document.getElementById('root')
-
-	);
+render(<App />, document.getElementById('root'));
