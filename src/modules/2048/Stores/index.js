581e265873a7a0a509db6f0f2835034a302e29ca
Komar0ff <binary.reptile@gmail.com>
2018-10-28 20:51:04 +0300

    added effector components

---
+++ src/modules/2048/Stores/index.js
@@ -0,0 +1,6 @@
+import { createStore } from 'effector'
+
+export const savedGames = createStore()
+export const currentGames = createStore()
+export const games = createStore()
+export const bestScore = createStore()
