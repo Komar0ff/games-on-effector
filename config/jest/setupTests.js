07f8123c9ba1e821902b367dc73d4fb5c4e52502
Komar0ff <binary.reptile@gmail.com>
2019-03-30 23:53:09 +0300

    update deps and remove enzyme

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -1,15 +1,8 @@
 import { JSDOM } from 'jsdom';
-import { configure, shallow, mount, render } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
 
 const exposedProperties = ['window', 'navigator', 'document'];
 const { document } = new JSDOM('').window;
 global.document = document;
-global.shallow = shallow;
-global.mount = mount;
-global.render = render;
 global.window = document.defaultView;
 global.HTMLElement = window.HTMLElement;
 global.HTMLAnchorElement = window.HTMLAnchorElement;
