6acbde0e5d63fe8515338ccd8a8b6681afd546ac
Ilya <10588170+Komar0ff@users.noreply.github.com>
2019-04-10 01:32:26 +0300

    small update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import locked from './assets/images/locked.svg';
 import { wrapper, block, blocked } from './style.module.scss';
 
 const App = () => {
 	return (
 		<div className={wrapper}>
-			<div className={blocked}> test </div>
 			<div className={block}> test </div>
+			<div className={blocked}> test </div>
 			<div className={blocked}> test </div>
 		</div>
 	);
