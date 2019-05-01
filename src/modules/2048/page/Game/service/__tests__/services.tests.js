68807dd57a3ef43cd209911cdd9ff719c57af4e1
Komar0ff <binary.reptile@gmail.com>
2019-05-01 12:27:08 +0300

    added random generation of active blocks

--- src/modules/2048/page/Game/service/__tests__/services.tests.js
+++ src/modules/2048/page/Game/service/__tests__/services.tests.js
@@ -1,16 +1,52 @@
-import { buttonClick } from '../index.js';
+import { newGame, mount, generation, random } from '../index.js';
 
 /**
  * Обработать нажатия на кнопки в панели (3 эвента)
  * обработка нажатий кнопок на клавиатуре
  * логика хранения для основного хранилища
  * логика хранения для скора
- * по нажатию на ньюгейм перерендериваетс окружение с рандомными данными
+ * по нажатию на ньюгейм перерендеривается окружение с рандомными данными [!]
+ * рандомная генерация активных блоков [done]
+ * отрисовка поля исходя из значений высоты и ширины [done]
  */
 
 describe('Services tests', () => {
-	it('button events', () => {
-		const newGameResult = buttonClick(0);
-		expect(newGameResult);
+	it('Drawing a playground based on height and width', () => {
+		const fakeResult = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
+		let result = mount(3, 4);
+		expect(result).toEqual(fakeResult);
+	});
+
+	it('Random generation of active blocks', () => {
+		let countActiveBlocks = 3; // No more than three
+		let playgroundWidth = 3;
+		let playgroundHeight = 4;
+
+		let generator = generation(countActiveBlocks, playgroundWidth, playgroundHeight);
+		let randomizer = random(countActiveBlocks, playgroundWidth, playgroundHeight);
+
+		expect(randomizer.length).toBe(3);
+		expect(randomizer[2].length).toBe(2);
+		expect(Number.isInteger(randomizer[0][0])).toBeTruthy();
+
+		expect(generator.length).toBe(playgroundHeight);
+		expect(generator[0].length).toBe(playgroundWidth);
+
+		let counterActiveBlock = 0;
+		for (let i = 0; i < playgroundHeight; i++) {
+			for (let j = 0; j < playgroundWidth; j++) {
+				if (generator[i][j] > 0) ++counterActiveBlock;
+			}
+		}
+
+		expect(counterActiveBlock).toBe(countActiveBlocks);
+	});
+
+	it('New game events', () => {
+		const fakeResult = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 16], [2, 0, 0, 0, 0]];
+
+		const newGameResult = newGame();
+		expect(newGameResult).toEqual(fakeResult);
 	});
 });
--- src/modules/2048/page/Game/service/index.js
+++ src/modules/2048/page/Game/service/index.js
@@ -3,6 +3,7 @@ import { createDomain } from 'effector';
 //domain
 export const gameDomain = createDomain('game page domain');
 
+export const mountEvent = gameDomain.event('mount event');
 export const newGameEvent = gameDomain.event('new game event');
 export const revertGameEvent = gameDomain.event('revert game event');
 export const savedGameEvent = gameDomain.event('saved game event');
@@ -11,5 +12,60 @@ export const moveEvent = gameDomain.event('move on playground');
 export const playgroundStore = gameDomain.store([]);
 export const scoreStore = gameDomain.store({ score: 0, bestScore: 0 });
 
-// export const buttonClick =
-//   newGameEvent.
+export const mount = (width, height) => {
+	let result = [];
+	for (let i = 0; i < height; i++) {
+		result.push([]);
+		for (let j = 0; j < width; j++) {
+			result[i].push(0);
+		}
+	}
+
+	return result;
+};
+
+export const random = (count, width, height) => {
+	// let index =
+	let output = [];
+
+	for (let i = 0; i < count; i++) {
+		let yСoordinate = Math.floor(Math.random() * height);
+		let xСoordinate = Math.floor(Math.random() * width);
+
+		output.push([xСoordinate, yСoordinate]);
+	}
+
+	return output;
+};
+
+export const generation = (count, width, height) => {
+	let result = [];
+	let activeBlocks = [];
+
+	for (let i = 0; i < count; i++) {
+		let yСoordinate = Math.floor(Math.random() * height);
+		let xСoordinate = Math.floor(Math.random() * width);
+
+		activeBlocks.push([xСoordinate, yСoordinate]);
+	}
+
+	for (let i = 0; i < height; i++) {
+		result.push([]);
+		for (let j = 0; j < width; j++) {
+			result[i].push(0);
+			for (let k = 0; k < activeBlocks.length; k++) {
+				if (activeBlocks[k][1] === i && activeBlocks[k][0] === j) result[i][j] = 8;
+			}
+		}
+	}
+
+	return result;
+};
+
+// playgroundStore.on(mount, (state, payload) => payload)
+
+const fakeResult = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 16], [2, 0, 0, 0, 0]];
+
+export const newGame = () => {
+	return fakeResult;
+};
