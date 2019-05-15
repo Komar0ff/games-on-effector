0b07f2d0e9f4c74329d2a6e72a490f3373c30f94
Komar0ff <binary.reptile@gmail.com>
2019-05-15 22:34:40 +0300

    restructure tests

--- src/modules/2048/page/Game/services/__tests__/services.tests.js
+++ src/modules/2048/page/Game/services/__tests__/services.tests.js
@@ -1,96 +1,129 @@
-import { mountEvent, newGameEvent } from '../events';
+import { mountEvent, newGameEvent, moveEvent } from '../events';
 import { $playground, generation, random, moving } from '../stores/playground';
 
-// TODO: fix problem with isolation.
-it('Drawing a playground based on height and width and random generation of active blocks', () => {
-	let countActiveBlocks = 3; // No more than three
+describe('Random and generation', () => {
+	let playgroundActiveBlocks = 3;
 	let playgroundWidth = 3;
 	let playgroundHeight = 4;
 
-	let generator = generation(countActiveBlocks, playgroundWidth, playgroundHeight);
-	let randomizer = random(countActiveBlocks, playgroundWidth, playgroundHeight);
+	it('Create random active blocks', () => {
+		let output = random(playgroundActiveBlocks, playgroundWidth, playgroundHeight);
+	
+		expect(output.length).toBe(playgroundActiveBlocks);
+		expect(output[0].length).toBe(2) // two coordinates
+		expect(Number.isInteger(output[0][0])).toBeTruthy(); // coordinate must be number
+	})
+	
+	it('Generating a playground', () => {
+		let generator = generation(playgroundActiveBlocks, playgroundWidth, playgroundHeight);
+	
+		expect(generator.length).toBe(playgroundHeight);
+		expect(generator[0].length).toBe(playgroundWidth);
 
-	expect(randomizer.length).toBe(3);
-	expect(randomizer[2].length).toBe(2);
-	expect(Number.isInteger(randomizer[0][0])).toBeTruthy();
-
-	expect(generator.length).toBe(playgroundHeight);
-	expect(generator[0].length).toBe(playgroundWidth);
-
-	let counterActiveBlock = 0;
-	for (let i = 0; i < playgroundHeight; i++) {
-		for (let j = 0; j < playgroundWidth; j++) {
-			if (generator[i][j] > 0) ++counterActiveBlock;
+		let counterActiveBlock = 0;
+		for (let i = 0; i < playgroundHeight; i++) {
+			for (let j = 0; j < playgroundWidth; j++) {
+				if (generator[i][j] > 0) ++counterActiveBlock;
+			}
 		}
-	}
 
-	expect(counterActiveBlock).toBe(countActiveBlocks);
-});
+		expect(counterActiveBlock).toBe(playgroundActiveBlocks);
+	})	
+})
 
-it('Playground mounting with empty playground', () => {
+describe('With localStorage tests', () => {
+	it('Playground mounting with empty playground', () => {
+		let localStorage = { playground: [], count: 2, width: 3, height: 4 }; //localStorage imitation
+		expect($playground.getState()).toEqual([]);
+
+		mountEvent(localStorage);
+	
+		let playgroundStore = $playground.getState(); // storage state after mounting
+		expect(playgroundStore.length).toBe(4);
+		expect(playgroundStore[0].length).toBe(3);
+	});
+
+	it.todo('TODO: fix problem with isolation.')
+	
+	it('Playground mounting with playground', () => {
+		let localStorage = { playground: [[0, 0, 8], [0, 1024, 0]], count: 2, width: 3, height: 2 }; //localStorage imitation
+		mountEvent(localStorage);
+
+		let playgroundStore = $playground.getState(); // storage state after mounting
+		expect(playgroundStore.length).toBe(2);
+		expect(playgroundStore[0].length).toBe(3);
+	});
+})
+
+
+it('New game event', () => {
 	let localStorage = { playground: [], count: 2, width: 3, height: 4 }; //localStorage imitation
-
-	expect($playground.getState()).toEqual([]);
-	mountEvent(localStorage);
-
-	let playgroundStore = $playground.getState(); // storage state after mounting
-	expect(playgroundStore.length).toBe(4);
-	expect(playgroundStore[0].length).toBe(3);
-});
-
-it('Playground mounting with playground', () => {
-	let playgroundStore = $playground.getState();
-	let localStorage = { playground: [[0, 0, 8], [0, 1024, 0]], count: 2, width: 3, height: 4 }; //localStorage imitation
-
-	mountEvent(localStorage);
-
-	playgroundStore = $playground.getState(); // storage state after mounting
-	expect(playgroundStore.length).toBe(2);
-	expect(playgroundStore[0].length).toBe(3);
-});
-
-it('newGame event', () => {
-	let localStorage = { playground: [], count: 2, width: 3, height: 4 };
-
 	newGameEvent(localStorage);
-	let playgroundStore = $playground.getState();
 
+	let playgroundStore = $playground.getState();
 	expect(playgroundStore.length).toBe(4);
 	expect(playgroundStore[0].length).toBe(3);
 });
 
-it('Arrow left move event', () => {
-	const oldState = [[0, 0, 8, 0], [8, 16, 8, 0], [0, 8, 8, 8]];
-	const newState = [[8, 0, 0, 0], [8, 16, 8, 0], [16, 8, 0, 0]];
-	const keyCode = 37;
+describe('Move events', () => {
+	it('Arrow left move event', () => {
+		const oldState = [[0, 0, 8, 0], [8, 16, 8, 0], [0, 8, 8, 8]];
+		const newState = [[8, 0, 0, 0], [8, 16, 8, 0], [16, 8, 0, 0]];
+		const keyCode = 37;
+	
+		let $ = moving(oldState, keyCode);
+		expect($).toEqual(newState);
+	});
+	
+	it('Arrow right move event', () => {
+		const oldState = [[2, 0, 0, 0], [8, 0, 0, 0], [0, 8, 8, 8]];
+		const newState = [[0, 0, 0, 2], [0, 0, 0, 8], [0, 0, 8, 16]];
+		const keyCode = 39;
+	
+		let $ = moving(oldState, keyCode);
+		expect($).toEqual(newState);
+	});
+	
+	it('Arrow up move event', () => {
+		const oldState = [[8, 0, 8, 8], [0, 0, 2, 0], [0, 8, 8, 8]];
+		const newState = [[8, 8, 8, 16], [0, 0, 2, 0], [0, 0, 8, 0]];
+		const keyCode = 38;
+	
+		let $ = moving(oldState, keyCode);
+		expect($).toEqual(newState);
+	});
+	
+	it('Arrow down move event', () => {
+		const oldState = [[0, 0, 8, 8], [0, 0, 2, 0], [0, 8, 8, 8]];
+		const newState = [[0, 0, 8, 0], [0, 0, 2, 0], [0, 8, 8, 16]];
+		const keyCode = 40;
+	
+		let $ = moving(oldState, keyCode);
+		expect($).toEqual(newState);
+	});
 
-	let $ = moving(oldState, keyCode);
-	expect($).toEqual(newState);
-});
+	it('After move event', () => {
+		let keyCode = 37
+		let localStorage = { playground: [[0, 0, 8], [0, 1024, 0]], count: 2, width: 3, height: 2 }; //localStorage imitation
 
-it('Arrow right move event', () => {
-	const oldState = [[2, 0, 0, 0], [8, 0, 0, 0], [0, 8, 8, 8]];
-	const newState = [[0, 0, 0, 2], [0, 0, 0, 8], [0, 0, 8, 16]];
-	const keyCode = 39;
+		mountEvent(localStorage);
+		moveEvent(keyCode)
 
-	let $ = moving(oldState, keyCode);
-	expect($).toEqual(newState);
-});
+		let playgroundStore = $playground.getState();
+		let playgroundHeight = localStorage.height
+		let playgroundWidth = localStorage.width
+		let playgroundActiveBlocks = localStorage.count+1
 
-it('Arrow up move event', () => {
-	const oldState = [[8, 0, 8, 8], [0, 0, 2, 0], [0, 8, 8, 8]];
-	const newState = [[8, 8, 8, 16], [0, 0, 2, 0], [0, 0, 8, 0]];
-	const keyCode = 38;
+		let counterActiveBlock = 0;
+		for (let i = 0; i < playgroundHeight; i++) {
+			for (let j = 0; j < playgroundWidth; j++) {
+				if (playgroundStore[i][j] > 0) ++counterActiveBlock;
+			}
+		}
 
-	let $ = moving(oldState, keyCode);
-	expect($).toEqual(newState);
-});
+		expect(counterActiveBlock).toBe(playgroundActiveBlocks);
+	})
+})
 
-it('Arrow down move event', () => {
-	const oldState = [[0, 0, 8, 8], [0, 0, 2, 0], [0, 8, 8, 8]];
-	const newState = [[0, 0, 8, 0], [0, 0, 2, 0], [0, 8, 8, 16]];
-	const keyCode = 40;
 
-	let $ = moving(oldState, keyCode);
-	expect($).toEqual(newState);
-});
+
