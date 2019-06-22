66303eb8392443609ebc93344bba55a84cf0ddae
Komar0ff <binary.reptile@gmail.com>
2019-06-22 17:43:39 +0300

    combined all the moves into one test

--- src/modules/2048/page/Game/services/__tests__/helpers.test.js
+++ src/modules/2048/page/Game/services/__tests__/helpers.test.js
@@ -1,49 +1,5 @@
 import { generation, random, equal, full, scoring, moving, winning } from '../helpers';
-
-const helpersMock = {
-	equalMock: {
-		firstState: [[0, 0, 8], [0, 1024, 0]],
-		secondState: [[0, 0, 8], [0, 1024, 2]]
-	},
-
-	fullMock: {
-		state: [[1, 4, 4], [3, 4, 5]]
-	},
-
-	scoringMock: {
-		state: [[1024, 0, 0], [16, 1024, 2]]
-	},
-
-	winningMock: {
-		state: [[16, 0, 0], [16, 2048, 2]]
-	}
-};
-
-const movingMock = {
-	left: {
-		oldState: [[0, 0, 8, 0], [8, 16, 8, 0], [0, 8, 8, 8]],
-		newState: [[8, 0, 0, 0], [8, 16, 8, 0], [16, 8, 0, 0]],
-		keyCode: 37
-	},
-
-	up: {
-		oldState: [[8, 0, 8, 8], [0, 0, 2, 0], [0, 8, 8, 8]],
-		newState: [[8, 8, 8, 16], [0, 0, 2, 0], [0, 0, 8, 0]],
-		keyCode: 38
-	},
-
-	right: {
-		oldState: [[2, 0, 0, 0], [8, 0, 0, 0], [0, 8, 8, 8]],
-		newState: [[0, 0, 0, 2], [0, 0, 0, 8], [0, 0, 8, 16]],
-		keyCode: 39
-	},
-
-	down: {
-		oldState: [[0, 0, 8, 8], [0, 0, 2, 0], [0, 8, 8, 8]],
-		newState: [[0, 0, 8, 0], [0, 0, 2, 0], [0, 8, 8, 16]],
-		keyCode: 40
-	}
-};
+import {helpersMock, movingMock} from '../__mocks__/helpers.mock.js'
 
 describe('Helpers', () => {
 	let playgroundActiveBlocks = 3;
@@ -89,30 +45,20 @@ describe('Helpers', () => {
 		expect(result).toBe(2066);
 	});
 
-	it('is there a 2048?', () => {
+	it('Is there a 2048?', () => {
 		const result = winning(helpersMock.winningMock.state);
 		expect(result).toBeTruthy();
 	});
-});
 
-describe('Move tests', () => {
-	it('Arrow left move event', () => {
+	it('Move left/right/up/down', () => {
 		let moveLeft = moving(movingMock.left.oldState, movingMock.left.keyCode);
-		expect(moveLeft).toEqual(movingMock.left.newState);
-	});
-
-	it('Arrow right move event', () => {
 		let moveRight = moving(movingMock.right.oldState, movingMock.right.keyCode);
-		expect(moveRight).toEqual(movingMock.right.newState);
-	});
-
-	it('Arrow up move event', () => {
 		let moveUp = moving(movingMock.up.oldState, movingMock.up.keyCode);
-		expect(moveUp).toEqual(movingMock.up.newState);
-	});
-
-	it('Arrow down move event', () => {
 		let moveDown = moving(movingMock.down.oldState, movingMock.down.keyCode);
+
+		expect(moveLeft).toEqual(movingMock.left.newState);
+		expect(moveRight).toEqual(movingMock.right.newState);
+		expect(moveUp).toEqual(movingMock.up.newState);
 		expect(moveDown).toEqual(movingMock.down.newState);
-	});
+	})
 });
