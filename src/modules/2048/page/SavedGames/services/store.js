d8110f9e19f676cd433c4bdc132eeb77da3d43fd
Komar0ff <binary.reptile@gmail.com>
2019-07-15 21:19:22 +0300

    refactoring SavedGames page

--- src/modules/2048/page/SavedGames/services/store.js
+++ src/modules/2048/page/SavedGames/services/store.js
@@ -8,12 +8,8 @@ export const $alert = createStore(false);
 $alert.on(alertEvent, (store) => true).watch(() => setTimeout(() => $alert.setState(false), 2000));
 
 export const $savedGames = createStore([]);
+
 $savedGames
 	.on(modalEvent, (store, payload) => (payload === 'yes' ? [] : store))
-	.on(removeCardEvent, (store, payload) => {
-		let _store = [...store];
-		_store.splice(payload, 1);
-
-		return _store;
-	})
+	.on(removeCardEvent, (store, payload) => [...store].splice(payload, 1))
 	.updates.watch(() => alertEvent());
