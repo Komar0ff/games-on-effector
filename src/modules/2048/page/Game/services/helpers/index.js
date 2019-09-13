b1e51eee3db4b8551db0fd18e8a88d392abbe90b
Ilya <10588170+Komar0ff@users.noreply.github.com>
2019-09-13 15:55:32 +0300

    Update index.js

--- src/modules/2048/page/Game/services/helpers/index.js
+++ src/modules/2048/page/Game/services/helpers/index.js
@@ -53,7 +53,7 @@ export const generation = (count, width, height) => {
 export const winning = (state) =>
 	state.find((value) => value.find((subValue) => subValue === 2048)) ? true : false;
 
-// todo
+// deprecated. the new mechanics are replacing this
 export const moving = (oldState, moveKey) => {
 	let newState = [...oldState];
 
