b7eb59a7583bcf1e3cfc0fcf5d10e4e6c596c973
Komar0ff <binary.reptile@gmail.com>
2019-09-17 10:45:10 +0300

    update tests

--- src/modules/2048/page/Game/services/mechanics/IndexIncrease/__tests__/indexIncrease.test.js
+++ src/modules/2048/page/Game/services/mechanics/IndexIncrease/__tests__/indexIncrease.test.js
@@ -1,117 +1,109 @@
 import IndexIncrease from '../index';
 
-let proportions = { width: 10, height: 20 };
+describe('Index increase tests', () => {
+	let xOffsetArray = [],
+		yOffsetArray = [];
 
-it('Index increase', () => {
-	const xOffsetArray = [
-		{ x: 0, y: 1, value: 2 },
-		{ x: 3, y: 1, value: 4 },
-		{ x: 4, y: 2, value: 8 },
-		{ x: 1, y: 2, value: 8 },
-		{ x: 2, y: 2, value: 8 },
-		{ x: 2, y: 0, value: 8 }
-	];
+	let xOffsetArraySubset = [],
+		xOffsetArraySubsetAfterMerge = [];
 
-	const indexIncreaseX = new IndexIncrease(xOffsetArray, 'x');
-	expect(
-		indexIncreaseX
-			.subsetFormation()
-			.findSameBlocksAndMerge()
-			.moveToFreeSpace(proportions.width)
-			.subsetIntegration()
-	).toEqual([
-		{ x: 9, y: 1, value: 2 },
-		{ x: 10, y: 1, value: 4 },
-		{ x: 9, y: 2, value: 8 },
-		{ x: 10, y: 2, value: 16 },
-		{ x: 10, y: 0, value: 8 }
-	]);
-});
+	let proportions = { width: 10, height: 20 };
 
-it('Subset formation', () => {
-	const xOffsetArray = [
-		{ x: 0, y: 1, value: 2 },
-		{ x: 3, y: 1, value: 4 },
-		{ x: 4, y: 2, value: 8 },
-		{ x: 1, y: 2, value: 8 },
-		{ x: 2, y: 2, value: 8 },
-		{ x: 2, y: 0, value: 8 }
-	];
+	beforeEach(() => {
+		xOffsetArray = [
+			{ x: 0, y: 1, value: 2 },
+			{ x: 3, y: 1, value: 4 },
+			{ x: 4, y: 2, value: 8 },
+			{ x: 1, y: 2, value: 8 },
+			{ x: 2, y: 2, value: 8 },
+			{ x: 2, y: 0, value: 8 }
+		];
 
-	const indexIncreaseX = new IndexIncrease(xOffsetArray, 'x');
+		yOffsetArray = [
+			{ x: 0, y: 1, value: 2 },
+			{ x: 3, y: 1, value: 4 },
+			{ x: 4, y: 2, value: 8 },
+			{ x: 1, y: 2, value: 8 },
+			{ x: 2, y: 2, value: 8 },
+			{ x: 2, y: 0, value: 8 }
+		];
 
-	expect(indexIncreaseX.subsetFormation().data).toEqual([
-		[{ x: 0, y: 1, value: 2 }, { x: 3, y: 1, value: 4 }],
-		[{ x: 1, y: 2, value: 8 }, { x: 2, y: 2, value: 8 }, { x: 4, y: 2, value: 8 }],
-		[{ x: 2, y: 0, value: 8 }]
-	]);
+		xOffsetArraySubset = [
+			[{ x: 0, y: 1, value: 2 }, { x: 3, y: 1, value: 4 }],
+			[{ x: 1, y: 2, value: 8 }, { x: 3, y: 2, value: 8 }, { x: 4, y: 2, value: 8 }],
+			[{ x: 2, y: 0, value: 8 }]
+		];
 
-	const yOffsetArray = [
-		{ x: 0, y: 1, value: 2 },
-		{ x: 3, y: 1, value: 4 },
-		{ x: 4, y: 2, value: 8 },
-		{ x: 1, y: 2, value: 8 },
-		{ x: 2, y: 2, value: 8 },
-		{ x: 2, y: 0, value: 8 }
-	];
+		xOffsetArraySubsetAfterMerge = [
+			[{ x: 0, y: 1, value: 2 }, { x: 3, y: 1, value: 4 }],
+			[{ x: 1, y: 2, value: 16 }, { x: 4, y: 2, value: 8 }],
+			[{ x: 2, y: 0, value: 8 }]
+		];
+	});
 
-	const indexIncreaseY = new IndexIncrease(yOffsetArray, 'y');
+	it('General', () => {
+		const indexIncreaseX = new IndexIncrease(xOffsetArray, 'x');
 
-	expect(indexIncreaseY.subsetFormation().data).toEqual([
-		[{ x: 0, y: 1, value: 2 }],
-		[{ x: 3, y: 1, value: 4 }],
-		[{ x: 4, y: 2, value: 8 }],
-		[{ x: 1, y: 2, value: 8 }],
-		[{ x: 2, y: 0, value: 8 }, { x: 2, y: 2, value: 8 }]
-	]);
-});
+		expect(
+			indexIncreaseX
+				.subsetFormation()
+				.findSameBlocksAndMerge()
+				.moveToFreeSpace(proportions.width)
+				.subsetIntegration()
+		).toEqual([
+			{ x: 9, y: 1, value: 2 },
+			{ x: 10, y: 1, value: 4 },
+			{ x: 9, y: 2, value: 8 },
+			{ x: 10, y: 2, value: 16 },
+			{ x: 10, y: 0, value: 8 }
+		]);
+	});
 
-it('Search and merge the same blocks', () => {
-	const xOffsetArray = [
-		[{ x: 0, y: 1, value: 2 }, { x: 3, y: 1, value: 4 }],
-		[{ x: 1, y: 2, value: 8 }, { x: 3, y: 2, value: 8 }, { x: 4, y: 2, value: 8 }],
-		[{ x: 2, y: 0, value: 8 }]
-	];
+	it('Subset formation', () => {
+		const indexIncreaseX = new IndexIncrease(xOffsetArray, 'x');
+		expect(indexIncreaseX.subsetFormation().data).toEqual([
+			[{ x: 0, y: 1, value: 2 }, { x: 3, y: 1, value: 4 }],
+			[{ x: 1, y: 2, value: 8 }, { x: 2, y: 2, value: 8 }, { x: 4, y: 2, value: 8 }],
+			[{ x: 2, y: 0, value: 8 }]
+		]);
 
-	const indexIncreaseX = new IndexIncrease(xOffsetArray, 'x');
+		const indexIncreaseY = new IndexIncrease(yOffsetArray, 'y');
+		expect(indexIncreaseY.subsetFormation().data).toEqual([
+			[{ x: 0, y: 1, value: 2 }],
+			[{ x: 3, y: 1, value: 4 }],
+			[{ x: 4, y: 2, value: 8 }],
+			[{ x: 1, y: 2, value: 8 }],
+			[{ x: 2, y: 0, value: 8 }, { x: 2, y: 2, value: 8 }]
+		]);
+	});
 
-	expect(indexIncreaseX.findSameBlocksAndMerge().data).toEqual([
-		[{ x: 0, y: 1, value: 2 }, { x: 3, y: 1, value: 4 }],
-		[{ x: 1, y: 2, value: 8 }, { x: 4, y: 2, value: 16 }],
-		[{ x: 2, y: 0, value: 8 }]
-	]);
-});
+	it('Search and merge the same blocks', () => {
+		const indexIncreaseX = new IndexIncrease(xOffsetArraySubset, 'x');
+		expect(indexIncreaseX.findSameBlocksAndMerge().data).toEqual([
+			[{ x: 0, y: 1, value: 2 }, { x: 3, y: 1, value: 4 }],
+			[{ x: 1, y: 2, value: 8 }, { x: 4, y: 2, value: 16 }],
+			[{ x: 2, y: 0, value: 8 }]
+		]);
+	});
 
-it('Moving to an empty space', () => {
-	const xOffsetArray = [
-		[{ x: 0, y: 1, value: 2 }, { x: 3, y: 1, value: 4 }],
-		[{ x: 1, y: 2, value: 16 }, { x: 4, y: 2, value: 8 }],
-		[{ x: 2, y: 0, value: 8 }]
-	];
+	it('Moving to an empty space', () => {
+		const indexIncrease = new IndexIncrease(xOffsetArraySubsetAfterMerge, 'x');
 
-	const indexIncrease = new IndexIncrease(xOffsetArray, 'x');
+		expect(indexIncrease.moveToFreeSpace(proportions.width).data).toEqual([
+			[{ x: 9, y: 1, value: 2 }, { x: 10, y: 1, value: 4 }],
+			[{ x: 9, y: 2, value: 16 }, { x: 10, y: 2, value: 8 }],
+			[{ x: 10, y: 0, value: 8 }]
+		]);
+	});
 
-	expect(indexIncrease.moveToFreeSpace(proportions.width).data).toEqual([
-		[{ x: 9, y: 1, value: 2 }, { x: 10, y: 1, value: 4 }],
-		[{ x: 9, y: 2, value: 16 }, { x: 10, y: 2, value: 8 }],
-		[{ x: 10, y: 0, value: 8 }]
-	]);
-});
-
-it('Combining subsets into a single array', () => {
-	const array = [
-		[{ x: 0, y: 1, value: 2 }, { x: 3, y: 1, value: 4 }],
-		[{ x: 1, y: 2, value: 16 }, { x: 4, y: 2, value: 8 }],
-		[{ x: 2, y: 0, value: 8 }]
-	];
-
-	const indexIncrease = new IndexIncrease(array, 'x');
-
-	expect(indexIncrease.subsetIntegration()).toEqual([
-		{ x: 0, y: 1, value: 2 },
-		{ x: 3, y: 1, value: 4 },
-		{ x: 1, y: 2, value: 16 },
-		{ x: 4, y: 2, value: 8 },
-		{ x: 2, y: 0, value: 8 }
-	]);
+	it('Combining subsets into a single array', () => {
+		const indexIncrease = new IndexIncrease(xOffsetArraySubsetAfterMerge, 'x');
+		expect(indexIncrease.subsetIntegration()).toEqual([
+			{ x: 0, y: 1, value: 2 },
+			{ x: 3, y: 1, value: 4 },
+			{ x: 1, y: 2, value: 16 },
+			{ x: 4, y: 2, value: 8 },
+			{ x: 2, y: 0, value: 8 }
+		]);
+	});
 });
