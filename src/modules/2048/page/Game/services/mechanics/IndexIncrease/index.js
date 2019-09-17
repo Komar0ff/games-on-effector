d14c0777f6d5f33b4af5408b0ddc4943959ce22d
Komar0ff <binary.reptile@gmail.com>
2019-09-17 10:24:48 +0300

    added index increase

---
+++ src/modules/2048/page/Game/services/mechanics/IndexIncrease/index.js
@@ -0,0 +1,64 @@
+export default class IndeIncrease {
+	constructor(data, coordinate) {
+		this.data = data;
+		this.coordinate = coordinate;
+	}
+
+	subsetFormation() {
+		let subsets = [];
+		let uniqueSecondCoordinate = new Set();
+		const searchCoordinate = this.coordinate === 'x' ? 'y' : 'x';
+
+		for (let i = 0; i < this.data.length; i++) {
+			uniqueSecondCoordinate.add(this.data[i][searchCoordinate]);
+		}
+
+		for (let value of uniqueSecondCoordinate) {
+			subsets.push(
+				this.data
+					.sort((a, b) => a[this.coordinate] - b[this.coordinate])
+					.filter((subValue) => subValue[searchCoordinate] === value)
+			);
+		}
+
+		this.data = subsets;
+		return this;
+	}
+
+	findSameBlocksAndMerge() {
+		for (let i = 0; i < this.data.length; i++) {
+			for (let j = this.data[i].length - 1; j > 0; j--) {
+				if (this.data[i][j].value === this.data[i][j - 1].value) {
+					this.data[i][j].value *= 2;
+					this.data[i].splice(j - 1, 1);
+					break;
+				}
+			}
+		}
+
+		return this;
+	}
+
+	moveToFreeSpace(size) {
+		for (let i = 0; i < this.data.length; i++) {
+			let _size = size;
+			for (let j = this.data[i].length - 1; j >= 0; j--) {
+				this.data[i][j][this.coordinate] = _size;
+				--_size;
+			}
+		}
+
+		return this;
+	}
+
+	subsetIntegration() {
+		let newArray = [];
+		for (let i = 0; i < this.data.length; i++) {
+			for (let j = 0; j < this.data[i].length; j++) {
+				newArray.push(this.data[i][j]);
+			}
+		}
+
+		return newArray;
+	}
+}
