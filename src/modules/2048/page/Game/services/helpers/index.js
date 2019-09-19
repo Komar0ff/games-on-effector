d3fa8241698455e088d085134274c9409e1bb172
Komar0ff <binary.reptile@gmail.com>
2019-09-19 12:02:39 +0300

    commit old mechanics

--- src/modules/2048/page/Game/services/helpers/index.js
+++ src/modules/2048/page/Game/services/helpers/index.js
@@ -54,120 +54,120 @@ export const winning = (state) =>
 	state.find((value) => value.find((subValue) => subValue === 2048)) ? true : false;
 
 // deprecated. the new mechanics are replacing this
-export const moving = (oldState, moveKey) => {
-	let newState = [...oldState];
+// export const moving = (oldState, moveKey) => {
+// 	let newState = [...oldState];
 
-	if (moveKey === 37) {
-		for (let i = 0; i < newState.length; i++) {
-			let _ = [];
-			for (let j = 0; j < newState[i].length; j++) {
-				if (newState[i][j]) _.push(newState[i][j]);
-			}
+// 	if (moveKey === 37) {
+// 		for (let i = 0; i < newState.length; i++) {
+// 			let _ = [];
+// 			for (let j = 0; j < newState[i].length; j++) {
+// 				if (newState[i][j]) _.push(newState[i][j]);
+// 			}
 
-			if (_ || _.length > 1) {
-				for (let k = 0; k < _.length; k++) {
-					if (_[k] == _[k + 1]) (_[k] += _[k + 1]), delete _[k + 1];
-				}
-			}
-			_ = _.filter(Boolean);
+// 			if (_ || _.length > 1) {
+// 				for (let k = 0; k < _.length; k++) {
+// 					if (_[k] == _[k + 1]) (_[k] += _[k + 1]), delete _[k + 1];
+// 				}
+// 			}
+// 			_ = _.filter(Boolean);
 
-			newState[i] = _;
-			while (newState[i].length != oldState[i].length) newState[i].push(0);
-		}
-	}
+// 			newState[i] = _;
+// 			while (newState[i].length != oldState[i].length) newState[i].push(0);
+// 		}
+// 	}
 
-	if (moveKey === 39) {
-		for (let i = 0; i < newState.length; i++) {
-			let _ = [];
-			for (let j = 0; j < newState[i].length; j++) {
-				if (newState[i][j]) _.push(newState[i][j]);
-			}
+// 	if (moveKey === 39) {
+// 		for (let i = 0; i < newState.length; i++) {
+// 			let _ = [];
+// 			for (let j = 0; j < newState[i].length; j++) {
+// 				if (newState[i][j]) _.push(newState[i][j]);
+// 			}
 
-			if (_ || _.length > 1) {
-				for (let k = _.length - 1; k > 0; k--) {
-					if (_[k] == _[k - 1]) (_[k] += _[k - 1]), delete _[k - 1];
-				}
-			}
-			_ = _.filter(Boolean);
+// 			if (_ || _.length > 1) {
+// 				for (let k = _.length - 1; k > 0; k--) {
+// 					if (_[k] == _[k - 1]) (_[k] += _[k - 1]), delete _[k - 1];
+// 				}
+// 			}
+// 			_ = _.filter(Boolean);
 
-			newState[i] = _;
-			while (newState[i].length != oldState[i].length) newState[i].unshift(0);
-		}
-	}
+// 			newState[i] = _;
+// 			while (newState[i].length != oldState[i].length) newState[i].unshift(0);
+// 		}
+// 	}
 
-	if (moveKey === 38) {
-		let checked = [];
+// 	if (moveKey === 38) {
+// 		let checked = [];
 
-		for (let i = 0; i < newState.length; i++) {
-			for (let j = 0; j < newState[i].length; j++) {
-				let _ = [];
-				let flag = checked.filter((value) => value === j);
+// 		for (let i = 0; i < newState.length; i++) {
+// 			for (let j = 0; j < newState[i].length; j++) {
+// 				let _ = [];
+// 				let flag = checked.filter((value) => value === j);
 
-				if (!flag.length) {
-					if (newState[i][j]) {
-						checked.push(j);
-						_.push(newState[i][j]);
-						for (let k = i + 1; k < newState.length; k++) {
-							if (newState[k][j]) _.push(newState[k][j]), (newState[k][j] = 0);
-						}
+// 				if (!flag.length) {
+// 					if (newState[i][j]) {
+// 						checked.push(j);
+// 						_.push(newState[i][j]);
+// 						for (let k = i + 1; k < newState.length; k++) {
+// 							if (newState[k][j]) _.push(newState[k][j]), (newState[k][j] = 0);
+// 						}
 
-						if (_ || _.length > 1) {
-							for (let k = 0; k < _.length; k++) {
-								if (_[k] == _[k + 1]) (_[k] += _[k + 1]), delete _[k + 1];
-							}
-						}
+// 						if (_ || _.length > 1) {
+// 							for (let k = 0; k < _.length; k++) {
+// 								if (_[k] == _[k + 1]) (_[k] += _[k + 1]), delete _[k + 1];
+// 							}
+// 						}
 
-						_ = _.filter(Boolean);
+// 						_ = _.filter(Boolean);
 
-						for (let f = 0; f < newState.length; f++) {
-							if (!_[f]) _[f] = 0;
-							newState[f][j] = _[f];
-						}
-					}
-				}
-			}
-		}
-	}
+// 						for (let f = 0; f < newState.length; f++) {
+// 							if (!_[f]) _[f] = 0;
+// 							newState[f][j] = _[f];
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
 
-	if (moveKey === 40) {
-		let checked = [];
+// 	if (moveKey === 40) {
+// 		let checked = [];
 
-		for (let i = newState.length - 1; i >= 0; i--) {
-			for (let j = newState[i].length - 1; j >= 0; j--) {
-				let _ = [];
-				let flag = checked.filter((value) => value === j);
+// 		for (let i = newState.length - 1; i >= 0; i--) {
+// 			for (let j = newState[i].length - 1; j >= 0; j--) {
+// 				let _ = [];
+// 				let flag = checked.filter((value) => value === j);
 
-				if (!flag.length) {
-					if (newState[i][j]) {
-						checked.push(j);
-						_.push(newState[i][j]);
+// 				if (!flag.length) {
+// 					if (newState[i][j]) {
+// 						checked.push(j);
+// 						_.push(newState[i][j]);
 
-						for (let k = i - 1; k >= 0; k--) {
-							if (newState[k][j]) _.push(newState[k][j]), (newState[k][j] = 0);
-						}
+// 						for (let k = i - 1; k >= 0; k--) {
+// 							if (newState[k][j]) _.push(newState[k][j]), (newState[k][j] = 0);
+// 						}
 
-						if (_ || _.length > 1) {
-							for (let k = 0; k < _.length; k++) {
-								if (_[k] == _[k + 1]) (_[k] += _[k + 1]), delete _[k + 1];
-							}
-						}
+// 						if (_ || _.length > 1) {
+// 							for (let k = 0; k < _.length; k++) {
+// 								if (_[k] == _[k + 1]) (_[k] += _[k + 1]), delete _[k + 1];
+// 							}
+// 						}
 
-						_ = _.filter(Boolean);
+// 						_ = _.filter(Boolean);
 
-						for (let f = newState.length - 1; f >= 0; f--) {
-							if (!_[f]) _[f] = 0;
-						}
+// 						for (let f = newState.length - 1; f >= 0; f--) {
+// 							if (!_[f]) _[f] = 0;
+// 						}
 
-						_ = _.reverse();
+// 						_ = _.reverse();
 
-						for (let f = newState.length - 1; f >= 0; f--) {
-							newState[f][j] = _[f];
-						}
-					}
-				}
-			}
-		}
-	}
+// 						for (let f = newState.length - 1; f >= 0; f--) {
+// 							newState[f][j] = _[f];
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
 
-	return newState;
-};
+// 	return newState;
+// };
