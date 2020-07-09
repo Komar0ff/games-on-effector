90c0de79077e9cf9316d587e89532e0ef314de1c
ilya_komarov <binary.reptile@gmail.com>
2020-07-09 12:19:37 +0300

    fix: webpack merge import

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,17 +1,17 @@
-const webpack = require('webpack')
-const { join } = require('path')
-const merge = require('webpack-merge')
-const config = require('./webpack.config.js')
+const webpack = require('webpack');
+const { join } = require('path');
+const { merge } = require('webpack-merge');
+const config = require('./webpack.config.js');
 
 module.exports = merge(config, {
-  mode: 'development',
-  devtool: 'eval-source-map',
+	mode: 'development',
+	devtool: 'eval-source-map',
 
-  devServer: {
-    hot: true,
-    port: 3001,
-    open: true,
-    compress: true,
-    https: false
-  }
-})
+	devServer: {
+		hot: true,
+		port: 3001,
+		open: true,
+		compress: true,
+		https: false,
+	},
+});
