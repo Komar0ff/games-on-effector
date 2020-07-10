bfddff869ac72d1f17a52b6d8aa26c0970446998
ilya_komarov <binary.reptile@gmail.com>
2020-07-10 11:49:37 +0300

    chore: move new mechanics to another directory

---
+++ src/modules/2048/page/Game/mechanics/__test__/index.test.js
@@ -0,0 +1,18 @@
+import {
+	subsetFormation,
+	subsetIntegration,
+	tileGeneration,
+	decreseMoveToFreeSpace,
+	decreseFindSameBlocksAndMerge,
+	increaseMoveToFreeSpace,
+	increaseFindSameBlocksAndMerge,
+} from '../index';
+
+describe('Base tests', () => {
+	it.todo('subsetFormation');
+	it.todo('subsetIntegration');
+});
+
+describe('Index decrease tests', () => {});
+
+describe('Index increase tests', () => {});
--- src/modules/2048/page/Game/stores/Playground/index.js
+++ src/modules/2048/page/Game/stores/Playground/index.js
@@ -3,6 +3,18 @@ import { createApi } from 'effector';
 import { gameDomain } from '../../domain';
 import { generation } from '../../../../helpers';
 
+import {
+	subsetFormation,
+	subsetIntegration,
+	tileGeneration,
+	decreseMoveToFreeSpace,
+	decreseFindSameBlocksAndMerge,
+	increaseMoveToFreeSpace,
+	increaseFindSameBlocksAndMerge,
+	decreaseFindSameBlocksAndMerge,
+	decreaseMoveToFreeSpace,
+} from '../../mechanics';
+
 export const $playground = gameDomain.store({
 	tiles: [],
 	cells: [],
@@ -51,117 +63,4 @@ function increase(state, coordinate) {
 		|> subsetIntegration
 		|> ((_) => tileGeneration(_, state))
 	);
-}
-
-// ---------- MECHANICS
-// TODO: refactor
-export function subsetFormation(tiles, coordinate) {
-	let subsets = [];
-	let uniqueSecondCoordinate = new Set();
-	const searchCoordinate = coordinate === 'x' ? 'y' : 'x';
-
-	for (let i = 0; i < tiles.length; i++) {
-		uniqueSecondCoordinate.add(tiles[i][searchCoordinate]);
-	}
-
-	for (let value of uniqueSecondCoordinate) {
-		subsets.push(
-			tiles
-				.filter((subValue) => subValue[searchCoordinate] === value) // фильтрацию лучше сделать при генерации
-				.sort((a, b) => a[coordinate] - b[coordinate])
-		);
-	}
-
-	return subsets;
-}
-
-export function subsetIntegration(tiles) {
-	let flat = [];
-	for (let i = 0; i < tiles.length; i++) {
-		for (let j = 0; j < tiles[i].length; j++) {
-			flat.push(tiles[i][j]);
-		}
-	}
-
-	return flat;
-}
-
-export function tileGeneration(tiles, { width, height }) {
-	let target = true;
-	let _tiles = [...tiles];
-	while (target) {
-		let xСoordinate = Math.floor(Math.random() * width);
-		let yСoordinate = Math.floor(Math.random() * height);
-
-		target = _tiles.find((tile) => tile.x === xСoordinate && tile.y === yСoordinate);
-
-		if (!target) {
-			_tiles.push({
-				x: xСoordinate,
-				y: yСoordinate,
-				value: 2,
-			});
-
-			return _tiles;
-		}
-	}
-}
-
-// ------------- IndexDecrease
-
-export function decreaseMoveToFreeSpace(tiles, coordinate) {
-	let _tiles = [...tiles];
-
-	for (let i = 0; i < tiles.length; i++) {
-		for (let j = 0; j < tiles[i].length; j++) {
-			tiles[i][j][coordinate] = j;
-		}
-	}
-
-	return _tiles;
-}
-
-export function decreaseFindSameBlocksAndMerge(tiles) {
-	let _tiles = [...tiles];
-	for (let i = 0; i < _tiles.length; i++) {
-		for (let j = 0; j < _tiles[i].length - 1; j++) {
-			if (_tiles[i][j].value === _tiles[i][j + 1].value) {
-				_tiles[i][j].value *= 2;
-				_tiles[i].splice(j + 1, 1);
-				break;
-			}
-		}
-	}
-
-	return _tiles;
-}
-
-// -------------- IndexIncrease
-
-export function increaseMoveToFreeSpace(tiles, size, coordinate) {
-	let _tiles = [...tiles];
-	for (let i = 0; i < _tiles.length; i++) {
-		let _size = size;
-		for (let j = _tiles[i].length - 1; j >= 0; j--) {
-			_tiles[i][j][coordinate] = _size;
-			--_size;
-		}
-	}
-
-	return _tiles;
-}
-
-export function increaseFindSameBlocksAndMerge(tiles) {
-	let _tiles = [...tiles];
-	for (let i = 0; i < _tiles.length; i++) {
-		for (let j = 0; j < _tiles[i].length - 1; j++) {
-			if (_tiles[i][j].value === _tiles[i][j + 1].value) {
-				_tiles[i][j].value *= 2;
-				_tiles[i].splice(j + 1, 1);
-				break;
-			}
-		}
-	}
-
-	return _tiles;
 }
