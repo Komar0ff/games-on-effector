e23ffcb15621dd4fa7f2d822947915b8990ca1a8
ilya_komarov <binary.reptile@gmail.com>
2020-07-15 19:49:55 +0300

    test: added subsetFormation test

--- src/modules/2048/page/Game/mechanics/__test__/index.test.js
+++ src/modules/2048/page/Game/mechanics/__test__/index.test.js
@@ -8,27 +8,24 @@ import {
 	increaseFindSameBlocksAndMerge,
 } from '../index';
 
-describe('subsetFormation', () => {
-	let tiles = [
-		{ x: 0, y: 2, value: 4 },
+let tiles = [
+	{ x: 0, y: 2, value: 4 },
+	{ x: 2, y: 1, value: 2 },
+	{ x: 2, y: 2, value: 8 },
+	{ x: 2, y: 0, value: 16 },
+];
+
+let resultTiles = [
+	[{ x: 0, y: 2, value: 4 }],
+	[
+		{ x: 2, y: 0, value: 16 },
 		{ x: 2, y: 1, value: 2 },
 		{ x: 2, y: 2, value: 8 },
-		{ x: 2, y: 2, value: 8 },
-		{ x: 2, y: 0, value: 16 },
-	];
+	],
+];
 
-	let resultTiles = [
-		[{ x: 0, y: 2, value: 4 }],
-		[
-			{ x: 2, y: 0, value: 16 },
-			{ x: 2, y: 1, value: 2 },
-			{ x: 2, y: 2, value: 8 },
-		],
-	];
-
-	it('should return an array of arrays of uniqal rows', () => {
-		expect(subsetFormation(tiles, 'y')).toEqual(resultTiles);
-	});
+it('should return an array of arrays of uniqal rows', () => {
+	expect(subsetFormation(tiles, 'y')).toEqual(resultTiles);
 });
 
 it.todo('subsetIntegration');
