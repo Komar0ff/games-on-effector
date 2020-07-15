c7011c392560df005f861bd415316652bd219406
ilya_komarov <binary.reptile@gmail.com>
2020-07-15 20:04:09 +0300

    test: added subsetIntegration test

--- src/modules/2048/page/Game/mechanics/__test__/index.test.js
+++ src/modules/2048/page/Game/mechanics/__test__/index.test.js
@@ -8,31 +8,52 @@ import {
 	increaseFindSameBlocksAndMerge,
 } from '../index';
 
-let tiles = [
-	{ x: 0, y: 2, value: 4 },
-	{ x: 2, y: 1, value: 2 },
-	{ x: 2, y: 2, value: 8 },
-	{ x: 2, y: 0, value: 16 },
-];
-
-let resultTiles = [
-	[{ x: 0, y: 2, value: 4 }],
-	[
-		{ x: 2, y: 0, value: 16 },
+describe('subsetFormation', () => {
+	let tiles = [
+		{ x: 0, y: 2, value: 4 },
 		{ x: 2, y: 1, value: 2 },
 		{ x: 2, y: 2, value: 8 },
-	],
-];
+		{ x: 2, y: 0, value: 16 },
+	];
 
-it('should return an array of arrays of uniqal rows', () => {
-	expect(subsetFormation(tiles, 'y')).toEqual(resultTiles);
+	let resultTiles = [
+		[{ x: 0, y: 2, value: 4 }],
+		[
+			{ x: 2, y: 0, value: 16 },
+			{ x: 2, y: 1, value: 2 },
+			{ x: 2, y: 2, value: 8 },
+		],
+	];
+
+	it('should return an array of arrays of uniqal rows', () => {
+		expect(subsetFormation(tiles, 'y')).toEqual(resultTiles);
+	});
 });
 
-it.todo('subsetIntegration');
-//  describe('Index decrease tests',  () => {
-//
-//  })
-//
-//  describe('Index increase tests',  () => {
-//
-//  })
+describe('subsetIntegration', () => {
+	let tiles = [
+		[
+			{ x: 0, y: 0, value: 4 },
+			{ x: 0, y: 1, value: 2 },
+		],
+		[
+			{ x: 1, y: 0, value: 4 },
+			{ x: 1, y: 1, value: 2 },
+		],
+		[
+			{ x: 2, y: 0, value: 2 },
+			{ x: 2, y: 1, value: 4 },
+		],
+	];
+
+	let result = [
+		{ x: 0, y: 0, value: 4 },
+		{ x: 0, y: 1, value: 2 },
+		{ x: 1, y: 0, value: 4 },
+		{ x: 1, y: 1, value: 2 },
+		{ x: 2, y: 0, value: 2 },
+		{ x: 2, y: 1, value: 4 },
+	];
+
+	expect(subsetIntegration(tiles)).toEqual(result);
+});
